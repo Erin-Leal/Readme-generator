6e328e76d946b3ec19fc6ab4a0a6f94c081d0ae3
Riagi15 <erinvleal@gmail.com>
2020-10-14 15:24:23 -0700

    Filled in the details with our questions, write to readme, and prompt functionalities. functional but readme output needs a couple adjustments.

--- Develop/index.js
+++ Develop/index.js
@@ -6,7 +6,109 @@ const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 //question object
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "Title",
+            message: "Title of your project?"
+        },
+        {
+            type: "input",
+            name: "Description",
+            message: "Describe the application."
+        },
+        {
+            type: "input",
+            name: "TableOfContents",
+            message: "The table of contents is?"
+        },
+        {
+            type: "input",
+            name: "Installation",
+            message: "How do you install the program?"
+        },
+        {
+            type: "input",
+            name: "Usage",
+            message: "How do you use it?"
+        },
+        {
+            type: "input",
+            name: "License",
+            message: "What kind of licence?"
+        },
+        {
+            type: "input",
+            name: "Contributing",
+            message: "Who developed the application?"
+        },
+        {
+            type: "input",
+            name: "Tests",
+            message: "Explain the tests you've run on it."
+        },
+        {
+            type: "input",
+            name: "Questions",
+            message: "Common questions?"
+        },
+        {
+            type: "input",
+            name: "Badge",
+            message: "Make some keyboard art."
+        }
+    ]);
+}
 
 // function to write README file
+function generateReadme(answers) {
+    return `${answers.Title}
+    +++++++++++++++++++++++++++++++++++++++++++++++++++
+    ${answers.Description}
+    
+    ---------------------------------------------------
+    
+    ${answers.TableOfContents}
+    
+    ---------------------------------------------------
+    
+    ${answers.Installation}
+    
+    ---------------------------------------------------
+    
+    ${answers.Usage}
+    
+    ---------------------------------------------------
+    
+    ${answers.License}
+    
+    ---------------------------------------------------
+    
+    ${answers.Contributing}
+    
+    ---------------------------------------------------
+    
+    ${answers.Tests}
+    
+    ---------------------------------------------------
+    
+    ${answers.Questions}
+    
+    +++++++++++++++++++++++++++++++++++++++++++++++++++
+    ${answers.Badge}`;
+}
 
-//Question prompt function
+    //Question prompt function
+    promptUser()
+        .then(function (answers) {
+            const readme = generateReadme(answers);
+
+            return writeFileAsync("README.md", readme);
+        })
+        .then(function () {
+            console.log("Successfully wrote to README.md");
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
